4bad44b0c9b480f285a0a3818918cb649b4e2679
Kartavya728 <kartavya.xenon@gmail.com>
2025-05-09 13:09:54 +0530

    del in

--- components/main/Events.tsx
+++ components/main/Events.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { StickyScroll, ContentItem } from "@/components/ui/sticky-scroll-reveal";
+import { StickyScroll, ContentItem } from "./Scroll";
 
 import { motion, Variants } from "framer-motion";
 
